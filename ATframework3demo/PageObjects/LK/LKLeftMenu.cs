65e6cc0cf1cca8fa07c5d0bcbe0b0c5f99d48074
Zmachinskii Dmitrii <zmachinskiiof@gmail.com>
2025-04-21 22:14:09 +0200

    Левое меню ЛК

---
+++ ATframework3demo/PageObjects/LK/LKLeftMenu.cs
@@ -0,0 +1,22 @@
+﻿using atFrameWork2.SeleniumFramework;
+using atFrameWork2.TestEntities;
+using OpenQA.Selenium;
+
+namespace ATframework3demo.PageObjects.LK
+{
+    public class LKLeftMenu
+    {
+        IWebDriver Driver { get; }
+
+        public LKLeftMenu(IWebDriver driver = default) 
+        {
+            Driver = driver;
+        }
+        WebItem myDataButton = new WebItem("//a[@href=\"#data\"]", "Поле перехода на таб мои данные");
+        WebItem myFestButton = new WebItem("//a[@href=\"#festivals\"]", "Кнопка перехода на таб мои фестивали");
+        WebItem participateButton = new WebItem("//a[@href=\"#participation\"]", "Кнопка перехода на таб участвую");
+        WebItem favoritesButton = new WebItem("//a[@href=\"#favorites\"]", "кнопка перехода на таб избранное");
+        WebItem notificationButton = new WebItem("//a[@href=\"#notifications\"]", "кнопка перехода на таб уведомления");
+        WebItem logoutButton = new WebItem("//button[@class=\"logout-btn\"]", "Кнопка выхода");
+    }
+}
